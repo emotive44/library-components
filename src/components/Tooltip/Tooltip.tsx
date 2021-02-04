9c79ebae19d2aabb5be1b4ca8bbf75f727ded00a
emotive44 <marko_streleshki_96@abv.bg>
2021-02-04 22:55:14 +0200

    fix tooltip component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,5 +1,4 @@
-import React, { FC } from 'react';
-
+import React, { FC, useEffect, useRef, useState } from 'react';
 import classes from './Tooltip.module.css';
 
 
@@ -13,6 +12,9 @@ const Tooltip: FC<TooltipProps> = ({
   message,
   children
 }) => {
+  const msgRef = useRef(null);
+  const [msgWidth, setMsgWidth] = useState(0);
+  const [styles, setStyles] = useState<any>({});
   const mainClasses = [classes.tooltip];
 
   //Set position of the tooltip
@@ -32,11 +34,50 @@ const Tooltip: FC<TooltipProps> = ({
     mainClasses.push(classes['position-top']);
   }
 
+  useEffect(() => {
+    let el: any = msgRef.current;
+    setMsgWidth(el.getBoundingClientRect().width + 10);
+
+    // set inital styles to have animation for show and hide
+    if(position === 'right') {
+      setStyles({ left: `${msgWidth - 50}px` });
+    }
+
+    if(position === 'left') {
+      setStyles({ right: `${msgWidth - 50}px` });
+    }
+  }, [msgWidth, position]);
+
+  const showToolTip = () => {
+    if(position === 'right') {
+      setStyles({ left: `${msgWidth}px` });
+    }
+
+    if(position === 'left') {
+      setStyles({ right: `${msgWidth}px` });
+    }
+  }
+
+  const hideToolTip = () => {
+    if(position === 'right') {
+      setStyles({ left: `${msgWidth - 50}px` });
+    }
+
+    if(position === 'left') {
+      setStyles({right: `${msgWidth - 50}px`});
+    }
+  }
+
   return (
-    <div className={mainClasses.join(' ')}>
+    <div 
+      ref             = {msgRef}
+      onMouseEnter    = {showToolTip}
+      onMouseLeave    = {hideToolTip} 
+      className       = {mainClasses.join(' ')} 
+    >
       <div className={classes.children}>
         {children}
-        <span className={classes.message}>
+        <span className={classes.message} style={styles}>
           {message}
         </span>
       </div>
